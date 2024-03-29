c8cca5892f8f693915f46bd2ebfdd0cedd483c2f
jacobbies <44556842+jacobbies@users.noreply.github.com>
2024-03-29 01:15:37 -0700

    .

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from dotenv import load_dotenv
-from apify_client import ApifyClient
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
-
-# Load .env variables and initialize apifyclient with API token
-load_dotenv()
-APIFY_API_KEY = os.getenv('APIFY_API_KEY')
-client = ApifyClient(APIFY_API_KEY)
 
 # Draw a title and some text to the app:
 '''
